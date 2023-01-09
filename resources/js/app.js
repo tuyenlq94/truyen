9b5e7c80c390535f92e32171e5b9fb14411764be
tuyenlq94 <tuyenicolor@gmail.com>
2023-01-09 14:56:37 +0700

    Tạo menu admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,6 +25,3 @@ require( './bootstrap' );
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue( {
-    el: '#app',
-} );
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,54 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+	<div class="row justify-content-center">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">{{ __('Trang quản lý') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+				<div class="card-body">
+					@if (session('status'))
+						<div class="alert alert-success" role="alert">
+							{{ session('status') }}
+						</div>
+					@endif
+					<nav class="navbar navbar-expand-lg bg-body-tertiary">
+						<div class="container-fluid">
+							<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="{{route('home')}}">Admin</a>
+								</li>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										Danh mục truyện
+									</a>
+									<ul class="dropdown-menu">
+										<li><a class="dropdown-item" href="#">Thêm danh mục</a></li>
+										<li><a class="dropdown-item" href="#">List danh mục</a></li>
+									</ul>
+								</li>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										Truyện sách
+									</a>
+									<ul class="dropdown-menu">
+										<li><a class="dropdown-item" href="#">Thêm sách truyện</a></li>
+										<li><a class="dropdown-item" href="#">List sách truyện</a></li>
+									</ul>
+								</li>
+							</ul>
+							<form class="d-flex" role="search">
+								<input class="form-control me-2" type="search" placeholder="Tìm kiếm" aria-label="Search">
+								<button class="btn btn-outline-success" type="submit">Tìm kiếm</button>
+							</form>
+							</div>
+						</div>
+					</nav>
 
-                    {{ __('Bạn đã đăng nhập thành công!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
